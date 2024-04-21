004c69a3fadae3cdddfa1ef517d07584c062513e
Akash <AwesomeAkash47@gmail.com>
2024-04-21 22:14:04 +0530

    cors giving headache

--- backend/old/main.py
+++ backend/old/main.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask, request, jsonify
 from google_auth_oauthlib.flow import InstalledAppFlow
 import json
@@ -7,7 +8,18 @@ import youtubepy
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+@app.after_request
+def add_headers(response):
+    response.headers.add('Content-Type', 'application/json')
+    response.headers.add('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers', 'content-type, traceid, withcredentials')
+    response.status=200
+    logging.getLogger('demo2').info('into add_headers')
+    return response
+
+
 
 '''def main():
     try:
@@ -42,8 +54,11 @@ def playlisturl():
 
 @app.route('/api/signin', methods=['POST'])
 def signin():
-    id_token = request.json.get('idToken')
-    playlistinfo = request.json.get('playlistInfo')
+    #id_token = request.json.get('idToken')
+    #playlistinfo = request.json.get('playlistInfo')
+
+    id_token = "4/0AeaYSHB3O0fL2mqo_ww53a6qeZo__5Gb5TBKkrG5SpR28_M2d4JgS_sLskvnVafO3_JgWw"
+    playlistinfo = {"playlist_name":"pretty playlist", "playlist_desc": "ayaya", "songs": ["if i dont like you Lily williams", "Young Love Ada LeAan"]}
 
     # Exchange ID token for OAuth 2.0 credentials
     flow = InstalledAppFlow.from_client_secrets_file(
