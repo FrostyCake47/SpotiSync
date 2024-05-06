04799b24e99d779d6d88ac3009aaf38d6cd561dd
Akash <AwesomeAkash47@gmail.com>
2024-05-06 17:23:21 +0530

    removed unused tags in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import Providers from "./components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-        <body className={inter.className}><GoogleOAuthProvider clientId={clientid}>{children}</GoogleOAuthProvider></body>
+        <body className={inter.className}>{children}</body>
       </Providers>
     </html>
   );
