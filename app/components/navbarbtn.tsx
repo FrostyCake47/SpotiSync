82ce2aef46ed08d28571ff42edfd2947d485188c
Akash <AwesomeAkash47@gmail.com>
2024-07-19 22:45:19 +0530

    added FAQ linking

--- app/components/navbarbtn.tsx
+++ app/components/navbarbtn.tsx
@@ -6,8 +6,9 @@ const Navbarbtn = (props: {name:string}) => {
     const {name} = props;
   return (
     <div className='px-1 sm:px-5 py-2 mx-2 cursor-pointer duration-200 hover:text-amber-500'>
-        {name != 'Contacts' && <Link className='text-[12px] sm:text-[18px]' href={"/" + (name == 'Home' ? '/' : name.toLowerCase())}>{name}</Link>}
+        {name == 'Home' && <Link className='text-[12px] sm:text-[18px]' href={'/home'}>{name}</Link>}
         {name == 'Contacts' && <a className='text-[12px] sm:text-[18px]' href='#contacts'>{name}</a>}
+        {name == 'FAQ' && <a className='text-[12px] sm:text-[18px]' href='#FAQ'>{name}</a>}
     </div>
   )
 }
