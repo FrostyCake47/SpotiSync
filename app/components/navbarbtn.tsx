4b056c6d3cacda38f1ff2dc16bc97e7199471fcf
Akash <AwesomeAkash47@gmail.com>
2024-07-20 11:37:24 +0530

    fixed FAQ ui

--- app/components/navbarbtn.tsx
+++ app/components/navbarbtn.tsx
@@ -6,7 +6,7 @@ const Navbarbtn = (props: {name:string}) => {
     const {name} = props;
   return (
     <div className='px-1 sm:px-5 py-2 mx-2 cursor-pointer duration-200 hover:text-amber-500'>
-        {name == 'Home' && <Link className='text-[12px] sm:text-[18px]' href={'/home'}>{name}</Link>}
+        {name == 'Home' && <Link className='text-[12px] sm:text-[18px]' href={'/'}>{name}</Link>}
         {name == 'Contacts' && <a className='text-[12px] sm:text-[18px]' href='#contacts'>{name}</a>}
         {name == 'FAQ' && <a className='text-[12px] sm:text-[18px]' href='#FAQ'>{name}</a>}
     </div>
